45c96007362cf78604424b38f12f668b270353b0
LethargicTactician <zhuran28@gmail.com>
2023-10-28 20:49:32 -0600

    api set up

--- FunctionalCoachScribe/Backend/dotnet-apigateway-ocelot/1.bat
+++ FunctionalCoachScribe/Backend/dotnet-apigateway-ocelot/1.bat
@@ -1,7 +1,7 @@
 CLS
 
-docker stop PRO290APIGatewayOcelot
-docker rm PRO290APIGatewayOcelot
-docker rmi pro290ocelotgatewayapi:1
-docker build --no-cache -t pro290ocelotgatewayapi:1 .
-docker run -d -p 5041:80 --name PRO290APIGatewayOcelot --net netPRO290 pro290ocelotgatewayapi:1
+docker stop CAPAPIGatewayOcelot
+docker rm CAPAPIGatewayOcelot
+docker rmi capocelotgatewayapi:1
+docker build --no-cache -t capocelotgatewayapi:1 .
+docker run -d -p 5041:80 --name CAPAPIGatewayOcelot --net netCapstone capocelotgatewayapi:1
