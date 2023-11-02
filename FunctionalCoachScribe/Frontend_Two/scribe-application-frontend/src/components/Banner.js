a83c8590d84d984fa179521a34510af160eb8446
LethargicTactician <zhuran28@gmail.com>
2023-11-02 17:38:50 -0600

    banner animation implemented

--- FunctionalCoachScribe/Frontend_Two/scribe-application-frontend/src/components/Banner.js
+++ FunctionalCoachScribe/Frontend_Two/scribe-application-frontend/src/components/Banner.js
@@ -1,17 +1,20 @@
 import { useState, useEffect } from 'react';
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/img/Xiao_Filler_Img.png";
+import headerImg from "../assets/img/Xiao_Filler_Img2.png";
+
+
 
 export const Banner = ()=>{
     //---------FOR ANIMATION-------
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting ] = useState(false);
-    const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer"];
+    const toRotate = [ "an adeptus", "Xiao", "your favorite coach"];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000;
 
+    
     useEffect(() => {
         let ticker = setInterval(() => {
             tick();
@@ -35,7 +38,7 @@ export const Banner = ()=>{
             setIsDeleting(true);
             setDelta(period);
 
-        } else if(isDeleting && updatedText == ''){
+        } else if(isDeleting && updatedText === ''){
             setIsDeleting(false);
             setLoopNum(loopNum + 1);
             setDelta(500);
@@ -51,13 +54,13 @@ export const Banner = ()=>{
                 <Row className="align-items-center">
                     <Col xs={12} md={5} xl={7}>
                         <span className="tagline" >Welcome to this thing</span>
-                        <h1>{"hi Im webcoded "}<span className="wrap">{text}</span></h1>
+                        <h1>{"Hi! I'm "}<span className="wrap">{text}</span></h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         <button onClick={() => console.log('connect')}>Let's chat <ArrowRightCircle size={25}/></button>
                     
                     </Col>
                     <Col xa={12} md={12} xl={5}>
-                        <img src={headerImg} alst="Header Img"></img>
+                        <img src={headerImg} alt="Header Img"></img>
                     </Col>
                 </Row>
             </Container>
