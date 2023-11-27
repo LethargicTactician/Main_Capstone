a62dafd44014d775063a74098db5221acdb7210a
LethargicTactician <zhuran28@gmail.com>
2023-11-26 19:54:14 -0700

    staff exists

--- FunctionalCoachScribe/Backend/userServiceAPI/src/main/java/gonzalez/capstone/userServiceAPI/repository/StaffRepository.java
+++ FunctionalCoachScribe/Backend/userServiceAPI/src/main/java/gonzalez/capstone/userServiceAPI/repository/StaffRepository.java
@@ -4,7 +4,6 @@ import java.util.UUID;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import gonzalez.capstone.userServiceAPI.models.Staff;
-import gonzalez.capstone.userServiceAPI.models.Teachers;
 import gonzalez.capstone.userServiceAPI.models.Users;
 
 @Repository
