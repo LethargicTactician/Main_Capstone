8da040a5aeb458f1c581b33ce541dcd4aa9f89a6
LethargicTactician <zhuran28@gmail.com>
2023-11-26 19:55:03 -0700

    staff exists

--- FunctionalCoachScribe/Backend/userServiceAPI/src/main/java/gonzalez/capstone/userServiceAPI/controller/UserRestController.java
+++ FunctionalCoachScribe/Backend/userServiceAPI/src/main/java/gonzalez/capstone/userServiceAPI/controller/UserRestController.java
@@ -354,15 +354,15 @@ public class UserRestController {
         userRepository.deleteById(userUUID);
     }
 
-    private static int createRandomIntBetween(int start, int end) {
-        return start + (int) Math.round(Math.random() * (end - start));
-    }
+    // private static int createRandomIntBetween(int start, int end) {
+    //     return start + (int) Math.round(Math.random() * (end - start));
+    // }
 
-    private static LocalDate createRandomDate(int startYear, int endYear) {
-        int day = createRandomIntBetween(1, 28);
-        int month = createRandomIntBetween(1, 12);
-        int year = createRandomIntBetween(startYear, endYear);
-        return LocalDate.of(year, month, day);
-    }
+    // private static LocalDate createRandomDate(int startYear, int endYear) {
+    //     int day = createRandomIntBetween(1, 28);
+    //     int month = createRandomIntBetween(1, 12);
+    //     int year = createRandomIntBetween(startYear, endYear);
+    //     return LocalDate.of(year, month, day);
+    // }
     //#endregion Basic crud for users
 }
