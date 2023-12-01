e9c19a3140ea191b3ae78c3fd1467bf65dd7b362
LethargicTactician <zhuran28@gmail.com>
2023-12-01 16:19:31 -0700

    blah

--- FunctionalCoachScribe/Backend/userServiceAPI/src/main/java/gonzalez/capstone/userServiceAPI/UserServiceApiApplication.java
+++ FunctionalCoachScribe/Backend/userServiceAPI/src/main/java/gonzalez/capstone/userServiceAPI/UserServiceApiApplication.java
@@ -2,7 +2,7 @@ package gonzalez.capstone.userServiceAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+// import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
